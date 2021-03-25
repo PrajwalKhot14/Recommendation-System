471070c61775f755310a92b1a446d00b2937bee5
Prajwal <70838537+PrajwalKhot14@users.noreply.github.com>
2021-03-25 20:17:41 +0530

    Update movie_recommender.py

--- movie_recommender.py
+++ movie_recommender.py
@@ -14,10 +14,10 @@ def get_index_from_title(title):
     return df[df.title == title]["index"].values[0]
 
 def combined_features(row):
-    return row['keywords'] + " " + row['cast'] + " " + row['genres'] + " " + row['director'] 
+    return row['cast'] + " " + row['genres'] + " " + row['director'] 
 
 df = pd.read_csv(r'movie_dataset.csv')
-features = ['keywords', 'cast', 'genres', 'director']
+features = ['cast', 'genres', 'director']
 for feature in features:
     df[feature] = df[feature].fillna(' ')
 
