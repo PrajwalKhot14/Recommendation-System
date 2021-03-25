04eb28c8dbfa8e0e712194423dc50e528eb68005
Prajwal <70838537+PrajwalKhot14@users.noreply.github.com>
2021-03-25 19:24:16 +0530

    Delete cosine-similarity.py

--- cosine-similarity.py
+++
@@ -1,9 +0,0 @@
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
-
-text = ["London Paris London", "Paris Paris London"]
-cv = CountVectorizer()
-count_matrix = cv.fit_transform(text)
-
-similarity_score = cosine_similarity(count_matrix)
-print(similarity_score)
