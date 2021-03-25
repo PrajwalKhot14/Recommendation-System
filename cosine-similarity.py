08e2a1ffdc2dce08692dd3c80dcfde4823bdb12c
Prajwal <khotprajwal04@gmail.com>
2021-03-25 18:17:04 +0530

    Similarity score
    Added function to calculate similarity score

--- cosine-similarity.py
+++ cosine-similarity.py
@@ -1,6 +1,9 @@
+from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
 text = ["London Paris London", "Paris Paris London"]
 cv = CountVectorizer()
 count_matrix = cv.fit_transform(text)
-print(count_matrix.toarray())
+
+similarity_score = cosine_similarity(count_matrix)
+print(similarity_score)
